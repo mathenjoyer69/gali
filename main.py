90b288b634d5aa744885ebba5bd57fa82381eac0
ariel <arielcalif10@gmail.com>
2025-11-12 19:52:09 +0200

    yola

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Button:
         self.rect = pygame.Rect(x, y, width, height)
         self.color = color
         self.original_color = color
-        self.variable = variable
+        self.variable = variable # this changes everytime you click the button sp u can keep track of the game state
 
     def draw(self, surface):
         self.color = self.original_color if self.variable else (255, 255, 255)
@@ -19,58 +19,60 @@ class Button:
         return self.rect.collidepoint(pos)
 
 class Simulation:
-    def __init__(self, screen, width, height, speed, matrix):
+    def __init__(self, screen, matrix):
         self.screen = screen
-        self.width = width
-        self.height = height
-        self.speed = speed
         self.matrix = matrix
-        self.type = 1
-        self.button = Button(0, 0, 40, 40, True, (120, 120, 120))
-        self.button1 = Button(100, 0, 40, 40, True, (222, 186, 69))
-        self.button2 = Button(200, 0, 40, 40, True, (120, 120, 120))
+        self.type = 1 # default block type to place
+        self.buttons = [Button(0, 0, 40, 40, True, (120, 120, 120)),
+                        Button(200, 0, 40, 40, True, (120, 120, 120)),
+                        Button(100, 0, 40, 40, True, (222, 186, 69))
+                        ]
+
         self.clock = pygame.time.Clock()
         self.clock.tick(60)
-        self.update_interval = 100
+        self.update_interval = 100 # we use this to call the update function every 100 ms without this variable the simulation would run too fast
         self.last_update_time = 0
-        self.update_hold = 50
+        self.update_hold = 50 # this is used so when u hold the mouse button it doesnt place blocks too fast
         self.last_update_time_hold = 0
 
     def update(self):
         current_time = pygame.time.get_ticks()
-        if current_time - self.last_update_time >= self.update_interval:
+        if current_time - self.last_update_time >= self.update_interval: # check if 100 ms have passed and then updates the blocks
             self.last_update_time = current_time
-            if self.button.variable:
+            if self.buttons[0].variable: # only update if we are in simulation mode
                 n = len(self.matrix)
-                for i in range(n - 1, -1, -1):
+                for i in range(n - 1, -1, -1): # updates the matrix from bottom to top
                     for j in range(n):
                         block = self.matrix[i][j]
                         block.update(self.matrix)
 
     def draw(self, surface):
         surface.fill((0, 0, 0))
-        if self.button.variable:
+        if self.buttons[0].variable: # this variable checks if we are in the simulation mode or in the block selection mode
             for i in range(len(self.matrix)):
                 for j in range(len(self.matrix[1])):
                     block = self.matrix[i][j]
                     block.draw(surface)
         else:
-            self.button1.draw(surface)
-            self.button2.draw(surface)
+            self.buttons[1].draw(surface)
+            self.buttons[2].draw(surface)
 
-        self.button.draw(surface)
+        self.buttons[0].draw(surface)
 
     def add_block(self, b_type, pos):
+        list_x = pos[0]
+        list_y = pos[1]
+        if b_type == 0:
+            self.matrix[list_y][list_x] = AirBlock.AirBlock(list_x * 20, list_y * 20, (0, 0, 0), b_type)
+
         if self.matrix[pos[1]][pos[0]].type == 0:
             current_time = pygame.time.get_ticks()
             if current_time - self.last_update_time_hold >= self.update_hold:
                 self.last_update_time_hold = current_time
-                list_x = pos[0]
-                list_y = pos[1]
+
                 if b_type == 1:
                     self.matrix[list_y][list_x] = StoneBlock.StoneBlock(list_x * 20, list_y * 20, b_type)
                 if b_type == 2:
-                    print(pos)
                     self.matrix[list_y][list_x] = SandBlock.SandBlock(list_x * 20, list_y * 20, b_type)
 
     @staticmethod
@@ -79,7 +81,8 @@ class Simulation:
 
     def run(self):
         running = True
-        mouse_held = False
+        mouse_held_l = False
+        mouse_held_r = False
         while running:
             self.update()
             self.draw(self.screen)
@@ -88,40 +91,49 @@ class Simulation:
                     running = False
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 1:
-                        mouse_held = True
-                        if self.button.is_over(pygame.mouse.get_pos()):
-                            self.button.variable = not self.button.variable
-                        if self.button.variable and not self.button.is_over(pygame.mouse.get_pos()):
-                            print("Button pressed")
+                        mouse_held_l = True
+                        if self.buttons[0].is_over(pygame.mouse.get_pos()):
+                            self.buttons[0].variable = not self.buttons[0].variable
+                        if self.buttons[0].variable and not self.buttons[0].is_over(pygame.mouse.get_pos()):
                             self.add_block(self.type, self.get_mouse_index(pygame.mouse.get_pos()))
                         else:
-                            #sand button
-                            if self.button1.is_over(pygame.mouse.get_pos()):
-                                self.button.original_color = self.button1.color
-                                self.type = 2
-                            elif self.button2.is_over(pygame.mouse.get_pos()):
-                                self.button.original_color = self.button2.color
+                            if self.buttons[1].is_over(pygame.mouse.get_pos()):
+                                self.buttons[0].original_color = self.buttons[1].color
                                 self.type = 1
+                            elif self.buttons[2].is_over(pygame.mouse.get_pos()):
+                                self.buttons[0].original_color = self.buttons[2].color
+                                self.type = 2
+                    if event.button == 3:
+                        mouse_held_r = True
+                        if self.buttons[0].variable and not self.buttons[0].is_over(pygame.mouse.get_pos()):
+                            self.add_block(0, self.get_mouse_index(pygame.mouse.get_pos()))
+
 
                 if event.type == pygame.MOUSEBUTTONUP:
                     if event.button == 1:
-                        mouse_held = False
+                        mouse_held_l = False
+                    elif event.button == 3:
+                        mouse_held_r = False
 
-            if mouse_held and self.button.variable and not self.button.is_over(pygame.mouse.get_pos()):
+
+            if mouse_held_l and self.buttons[0].variable and not self.buttons[0].is_over(pygame.mouse.get_pos()):
                 mouse_pos = pygame.mouse.get_pos()
                 list_x, list_y = self.get_mouse_index(mouse_pos)
                 if 0 <= list_x < len(self.matrix[0]) and 0 <= list_y < len(self.matrix):
                     self.add_block(self.type, (list_x, list_y))
-
+            elif mouse_held_r and self.buttons[0].variable and not self.buttons[0].is_over(pygame.mouse.get_pos()):
+                mouse_pos = pygame.mouse.get_pos()
+                list_x, list_y = self.get_mouse_index(mouse_pos)
+                if 0 <= list_x < len(self.matrix[0]) and 0 <= list_y < len(self.matrix):
+                    self.add_block(0, (list_x, list_y))
 
             pygame.display.flip()
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((600, 600))
-    matrix =  [[AirBlock.AirBlock(i*20, j*20, (0, 0, 0), 0) for i in range(30)] for j in range(30)]
-
-    sim = Simulation(screen, 600, 600, 0.1, matrix)
+    screen = pygame.display.set_mode((800, 800))
+    matrix =  [[AirBlock.AirBlock(i*20, j*20, (0, 0, 0), 0) for i in range(40)] for j in range(40)]
+    sim = Simulation(screen, matrix)
     sim.run()
 
 main()
